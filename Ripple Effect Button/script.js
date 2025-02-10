dc79d47b6e3461c6d91763dcec37617c6d01c61f
Sri-Shivahari <148426687+Sri-Shivahari@users.noreply.github.com>
2025-02-10 18:23:38 +0530

    Add files via upload

---
+++ Ripple Effect Button/script.js
@@ -0,0 +1,11 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+    const x = event.pageX - btnEl.offsetLeft;
+    const y = event.pageY - btnEl.offsetTop;
+
+    console.log(x, y); // Logging values
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+});
